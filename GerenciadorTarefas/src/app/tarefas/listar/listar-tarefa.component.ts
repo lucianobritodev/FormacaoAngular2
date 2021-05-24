1762c4c892f6e429071bc1b15afdee792e5b8cd8
Luciano Brito <lucianobrito.dev@gmail.com>
2021-05-24 03:17:08 -0300

    updated listar-tarefa.component
    added async functions for validations fields, and updated GUI of system

--- GerenciadorTarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ GerenciadorTarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { TarefaService, Tarefa } from '../shared';
+import { TarefaService, Tarefa, Mensagens } from '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -10,8 +10,11 @@ import { TarefaService, Tarefa } from '../shared';
 export class ListarTarefaComponent implements OnInit {
 
   tarefas: Tarefa[] = [];
+  public mensagens: Mensagens = new Mensagens();
 
-   constructor(private tarefaService: TarefaService) { }
+   constructor(
+     private tarefaService: TarefaService,
+    ) { }
 
   ngOnInit(): void {
     this.tarefas = this.listarTodos();
@@ -21,4 +24,30 @@ export class ListarTarefaComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  async remover($event: any, tarefa: Tarefa): Promise<void> {
+    $event.preventDefault();
+    // confirm(`Deseja remover a tarefa ${tarefa.nome} ?`)
+    const result = await this.mensagens.alertYesNo(`Deseja remover a tarefa ${tarefa.nome}?`);
+    if (result) {
+      // tslint:disable-next-line: no-non-null-assertion
+      this.tarefaService.remover(tarefa.id!);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  async alterarStatus(tarefa: Tarefa) {
+    const result = await this.mensagens.alertConfirm(`Deseja alterar o status da tarefa ${tarefa.nome}?`);
+    if (result) {
+      // tslint:disable-next-line: no-non-null-assertion
+      this.tarefaService.alterarStatus(tarefa.id!);
+      this.tarefas = this.listarTodos();
+    } else {
+      // tslint:disable-next-line: no-non-null-assertion
+      this.tarefaService.alterarStatus(tarefa.id!);
+      // tslint:disable-next-line: no-non-null-assertion
+      this.tarefaService.alterarStatus(tarefa.id!);
+      this.tarefas = this.listarTodos();
+    }
+  }
 }
